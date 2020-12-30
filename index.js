8afd1bb0ff277bab2230ceeaf00cbb3bf015d35a
AbdurRakib960 <rakibabdur960git@gmail.com>
2020-12-31 03:31:20 +0600

    calculate done

---
+++ index.js
@@ -0,0 +1,75 @@
+// subTotal , total , tax;
+var subTotal = document.getElementById('subTotal');
+var total = document.getElementById('total');
+var tax = document.getElementById('tax'); 
+
+// product price decrease
+function decrease(price, inputId, priceId) {
+  var parseItem1 = parseInt(inputId.value);
+  parseItem1--;
+  inputId.value = parseItem1;
+  priceId.innerText = price * parseInt(inputId.value);
+  subTotal.innerText = parseInt(priceItem1.innerText) + parseInt(priceItem2.innerText);
+  tax.innerText = parseInt(subTotal.innerText/ 100);
+  total.innerText = parseInt(subTotal.innerText) + parseInt(tax.innerText);
+
+};
+
+// product price increase
+
+function increase(price, inputId, priceId) {
+  var parseItem2 = parseInt(inputId.value);
+  parseItem2++;
+  inputId.value = parseItem2;
+  priceId.innerText = price * parseInt(inputId.value);
+  subTotal.innerText = parseInt(priceItem1.innerText) + parseInt(priceItem2.innerText);
+  tax.innerText = parseInt(subTotal.innerText/ 100);
+  total.innerText = parseInt(subTotal.innerText) + parseInt(tax.innerText);
+}
+
+// product 1 price and amount calculate
+
+var decreaseItem1 = document.getElementById('decrease1');
+var increaseItem1 = document.getElementById('increase1');
+var inputItem1 = document.getElementById('increaseItem1');
+var priceItem1 = document.getElementById('productPrice1');
+
+decreaseItem1.addEventListener('click', function() {
+  if(inputItem1.value < 1){
+    alert('please increase item number');
+  }
+  else{
+    decrease(1219, inputItem1, priceItem1)
+  }
+  
+});
+
+increaseItem1.addEventListener('click', function() {
+  increase(1219, inputItem1, priceItem1);
+});
+
+// product 2 price and amount calculate
+
+var decreaseItem2 = document.getElementById('decrease2');
+var increaseItem2 = document.getElementById('increase2');
+var inputItem2 = document.getElementById('increaseItem2');
+var priceItem2 = document.getElementById('productPrice2');
+
+decreaseItem2.addEventListener('click', function() {
+  if(inputItem2.value < 1) {
+    alert('please increase item number');
+  }
+  else{
+    decrease(59, inputItem2, priceItem2);
+  }
+});
+
+increaseItem2.addEventListener('click', function() {
+  increase(59, inputItem2, priceItem2);
+});
+
+// cancel items...
+
+
+
+
